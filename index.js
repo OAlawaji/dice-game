d75980fad1e8cc41331bc578942b46ef10a57cf1
Omar Alawaji <107493858+OAlawaji@users.noreply.github.com>
2024-03-18 07:33:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const pressableArea = document.getElementById('pressable-area');
+pressableArea.addEventListener('click', function() {
+  // Your code here to handle the click event
+  console.log('Pressable area clicked!');
+});
